0ce1ab119bd86cb6a048c00071cb4808b7a32a63
Stevenalv123 <stevenalv222@gmail.com>
2024-10-12 15:37:20 -0600

    primer commit

---
+++ components/index/index.js
@@ -0,0 +1,11 @@
+document.getElementById("facebook").onclick = function() {
+    window.location.href = "https://www.facebook.com/profile.php?id=100092623928171";
+}
+
+document.getElementById("instagram").onclick = function() {
+    window.location.href = "https://www.instagram.com/steven.avendano.1804/";
+}
+
+document.getElementById("whatsapp").onclick = function() {
+    window.location.href = "https://wa.link/ukwy0t";
+}
